5e29fadeffdbfbcdc030954b97b6d059375a8cf4
DidDrog11 <dsimons1987@gmail.com>
2021-04-05 10:49:27 +0100

    raster script

--- scripts/project_library.R
+++ scripts/project_library.R
@@ -1,11 +1,27 @@
-library("here")
-library("tidyverse")
-library("rgdal")
-library("sf")
-library("ggmap")
-library("ggspatial")
-library("OpenStreetMap")
-library("googledrive")
-library("rgeos")
-library("grid")
-library("magrittr")
+if (!require("pacman")) install.packages("pacman")
+pkgs =
+  c("here",
+    "rgeos",
+    "ggspatial",
+    "rgdal",
+    "grid",
+    "tidyverse",
+    "magrittr",
+    "bib2df",
+    "googledrive",
+    "readxl",
+    "countrycode",
+    "ggimage",
+    "sf",
+    "OpenStreetMap",
+    "tmap",
+    "maptools",
+    "raster",
+    "mapview",
+    "ggmap",
+    "taxize",
+    "distill",
+    "magrittr",
+    "rvest"
+  )
+pacman::p_load(pkgs, character.only = T)
