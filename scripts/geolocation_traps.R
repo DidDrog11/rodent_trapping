bdf32385681e7536ed615735e0e49b5a81561a0d
DidDrog11 <dsimons1987@gmail.com>
2021-04-11 12:03:57 +0100

    updated data and scripts

--- scripts/geolocation_traps.R
+++ scripts/geolocation_traps.R
@@ -19,9 +19,7 @@ lalehun_traps <- trap_sites %>%
          lon = -1 * dg2dec(var = lon_DdM, Dg = "_", Min = "'"),
          lat = dg2dec(var = lat_DdM, Dg = "_", Min = "'")) %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
-  filter(village == "lalehun",
-         empty_morning != "na") %>%
-  drop_na(empty_morning)
+  filter(village == "lalehun")
 
 lalehun_rodents <- location_rodents %>%
   filter(village == "lalehun") %>%
@@ -40,6 +38,23 @@ proj4string(lalehun_test) <- CRS("+proj=longlat +datum=WGS84")  ## for example
 res <- spTransform(lalehun_test, CRS("+proj=utm +zone=29 ellps=WGS84"))
 res
   
+pal <- colorFactor(c("red", "blue"), domain = c(1, 2))
+
+distinct_lal_traps <- lalehun_traps %>%
+  distinct(geometry, .keep_all = T)
+
+leaflet(distinct_lal_traps) %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
+  addCircleMarkers(radius = ~ifelse(visit == 2, 3, 2),
+                   color = ~pal(visit),
+                   stroke = F,
+                   fillOpacity = 0.5,
+                   popup = paste(distinct_lal_traps$trap_number),
+                   popupOptions = popupOptions(closeOnClick = T))  %>%
+  addLegend("topright", pal = pal, values = ~visit,
+            title = "Study visit")
+
+
 # seilama -----------------------------------------------------------------
 
 seilama_traps <- trap_sites %>%
@@ -54,41 +69,8 @@ seilama_traps <- trap_sites %>%
 
 # lalehun ggmap -------------------------------------------------------------------
 
-lalehun_17 <- get_googlemap(center = c(-11.0803, 8.197533), zoom = 17, maptype = "hybrid")
-lalehun_16 <- get_googlemap(center = c(-11.0803, 8.197533), zoom = 16, maptype = "hybrid")
 
-traps_lalehun_17 <- ggmap(lalehun_17) +
-  geom_sf(data = lalehun_traps,
-          aes(geometry = geometry,
-              colour = rodent_trapped,
-              alpha = 0.2),
-          inherit.aes = F) +
-  labs(title = "Trap locations Lalehun near Panguma",
-       color = "Trap success",
-       alpha = NULL,
-       x = "Longitude",
-       y = "Latitude") +
-  theme_minimal() +
-  guides(alpha = F)
 
-traps_lalehun_16 <- ggmap(lalehun_16) +
-  geom_sf(data = lalehun_traps,
-          aes(geometry = geometry,
-              colour = rodent_trapped,
-              shape = grid_number,
-              alpha = 0.2),
-          inherit.aes = F) +
-  coord_sf(xlim = c(-11.083, -11.077), ylim = c(8.193, 8.202)) +
-  scale_colour_manual(values = c("orange", "purple")) +
-  scale_shape_manual(values = c(15, 16, 17, 18, 19, 7, 10)) +
-  labs(title = "Trap locations Lalehun near Panguma",
-       color = "Trap success",
-       shape = "Grid number",
-       alpha = NULL,
-       x = "Longitude",
-       y = "Latitude") +
-  theme_minimal() +
-  guides(alpha = F)
 
 ggsave(plot = traps_lalehun_16, path = here("reports", "figures"), filename = "lalehun_traps.png")
 
@@ -133,6 +115,12 @@ traps_seilama_16 <- ggmap(seilama_16) +
 
 ggsave(plot = traps_seilama_16, path = here("reports", "figures"), filename = "seilama_traps.png")
 
+seilama_19 <- get_googlemap(center = c(-11.193628469657279, 8.122285428353395), zoom = 19, maptype = "hybrid", color = "bw")
+ggmap(seilama_19) +
+  scale_x_continuous(n.breaks = 8) +
+  scale_y_continuous(n.breaks = 8) +
+  theme(panel.grid.minor = element_line(colour = "white"),
+        panel.ontop=TRUE, panel.background = element_rect(fill = NA))
 # lalehun OSM ---------------------------------------------------------------------
 
 lalehun_1_ul <- c(8.197, -11.08)
